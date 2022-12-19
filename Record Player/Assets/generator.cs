b8f79cc6d57fdf8080688afd09ad22cd6b83c5c7
DorkSoul <Luke.hallinan@hotmail.com>
2022-12-19 20:40:25 +0000

    generating records around table

--- Record Player/Assets/generator.cs
+++ Record Player/Assets/generator.cs
@@ -4,27 +4,25 @@ using UnityEngine;
 
 public class generator : MonoBehaviour
 {
-    public int loops = 10;
-    public GameObject prefab;
+    public int numrecs = 7;
+    public GameObject RecordPrefab;
     // Start is called before the first frame update
     void Start()
     {
-        int rad = 1;
-        for(int i = 1 ; i <= loops ; i ++)
+        float rad = 0.7f;
+            
+        float theta = Mathf.PI * 2.0f / ((float)numrecs);
+        for (int j = 0 ; j < numrecs ; j ++)
         {
-            int numrecs = 10;
-            float theta = Mathf.PI * 2.0f / ((float)numrecs);
-
-            float angle  = 5 * theta; 
-            float x = Mathf.Sin(angle) * rad * (i) * 1.1f;
-            float y = Mathf.Cos(angle) * rad * (i) * 1.1f;
-            GameObject g = GameObject.Instantiate<GameObject>(prefab);
-            g.transform.position = transform.TransformPoint(new Vector3(x,y, 0));
-            g.transform.parent = this.transform;
-            // g.transform.position = new Vector3(x, y, 0);
-            // g.GetComponent<Renderer>().material.color =
-            //     Color.HSVToRGB(i / (float) numrecs, 1, 1);
-            // g.transform.parent = this.transform;
+            float angle  = j * theta; 
+            float x = Mathf.Sin(angle) * (rad) * 1.1f;
+            float z = Mathf.Cos(angle) * (rad) * 1.1f;
+            GameObject record = GameObject.Instantiate<GameObject>(RecordPrefab);
+            record.transform.position = transform.TransformPoint(new Vector3(x, 0, z));
+            Transform label = record.transform.GetChild(0);
+            label.GetComponent<Renderer>().material.color =
+                Color.HSVToRGB(j / (float) numrecs, 1, 1);
+            record.transform.parent = this.transform;
         }
     }
 
